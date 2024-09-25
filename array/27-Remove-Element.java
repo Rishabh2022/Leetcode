7b88b5f1a9b365d7b5e01a6f5609b651810f7db2
Rishabh <78684570+Rishabh2022@users.noreply.github.com>
2024-09-26 02:32:11 +0530

    [27] [Time Beats: 100.00%] [Memory Beats: 83.31%] - LeetPush

--- array/27-Remove-Element.java
+++ array/27-Remove-Element.java
@@ -1,6 +1,8 @@
 class Solution {
     public int removeElement(int[] nums, int val) {
 
+        //https://leetcode.com/problems/remove-element/description/?envType=study-plan-v2&envId=top-interview-150
+
         int i=0,j=nums.length-1,c=0;
         while(i<=j){
             if(nums[j]==val) {
