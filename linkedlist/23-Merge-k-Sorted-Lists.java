5d9f3a38e671130a7114c29b2b85bdbc1af85f9a
Rishabh <78684570+Rishabh2022@users.noreply.github.com>
2024-10-04 10:22:19 +0530

    [23] [Time Beats: 75.69%] [Memory Beats: 67.85%] - LeetPush

---
+++ linkedlist/23-Merge-k-Sorted-Lists.java
@@ -0,0 +1,43 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+
+    //https://leetcode.com/problems/merge-k-sorted-lists/
+    public ListNode mergeKLists(ListNode[] lists) {
+
+        //the optimal approach to solve this question is using the priority queue
+        // Initiate a minheap based on the val of the node
+        PriorityQueue<ListNode> queue = new PriorityQueue<>((a, b) -> a.val - b.val);
+        
+        // put the first node of every list into queue
+        for (ListNode node : lists) {
+            if (node == null) continue;
+            queue.add(node);
+        }
+        
+        // dummy head to carry the result
+        ListNode dummyHead = new ListNode(0);
+        ListNode curr = dummyHead;
+        
+        // link the root of minheap to he dummyHead, add the root.next to the minheap, repeate this process untill minheap runs out of nodes
+        while(!queue.isEmpty()) {
+            curr.next = queue.poll();
+            curr = curr.next;
+            if (curr.next != null) queue.add(curr.next);
+        }
+        
+        return dummyHead.next;
+
+
+        
+        
+    }
+}
