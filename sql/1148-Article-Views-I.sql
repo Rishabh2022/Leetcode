fd8a821be8ec4166e9033a00dbe1311debb3d846
Rishabh <78684570+Rishabh2022@users.noreply.github.com>
2024-10-05 23:07:20 +0530

    [1148] [Time Beats: 25.53%] - LeetPush

---
+++ sql/1148-Article-Views-I.sql
@@ -0,0 +1,5 @@
+# Write your MySQL query statement below
+
+#https://leetcode.com/problems/article-views-i/?envType=study-plan-v2&envId=top-sql-50
+
+select distinct author_id as id from Views where author_id=viewer_id order by author_id asc;
