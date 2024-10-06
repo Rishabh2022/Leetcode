613632f34ac1e479ed8b8b314983679e19d37c14
Rishabh <78684570+Rishabh2022@users.noreply.github.com>
2024-10-06 06:28:29 +0530

    [197] [Time Beats: 32.05%] - LeetPush

---
+++ sql/197-Rising-Temperature.sql
@@ -0,0 +1,7 @@
+# Write your MySQL query statement below
+
+#https://leetcode.com/problems/rising-temperature/description/?envType=study-plan-v2&envId=top-sql-50
+
+SELECT w1.id
+FROM Weather AS w1 , Weather AS w2
+WHERE w1.Temperature > w2.Temperature AND DATEDIFF(w1.recordDate , w2.recordDate) = 1
