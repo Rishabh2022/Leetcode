6a563214f1f69b3fef2cedeb8bcd033334ac0206
Rishabh <78684570+Rishabh2022@users.noreply.github.com>
2024-10-07 00:25:10 +0530

    [100] [Time Beats: 100.00%] [Memory Beats: 69.80%] - LeetPush

---
+++ tree/100-Same-Tree.java
@@ -0,0 +1,40 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public boolean isSameTree(TreeNode p, TreeNode q) {
+
+        //https://leetcode.com/problems/same-tree/
+
+
+        // if(p==null || q==null) return p==q;
+        // return (p.val==q.val) && isSameTree(p.left,q.left) && isSameTree(p.right,q.right);
+
+
+        // Base case: if both trees are null, they are identical
+        if (p == null && q == null) {
+            return true;
+        }
+        // If only one tree is null or the values are different, they are not identical
+        if (p == null || q == null || p.val != q.val) {
+            return false;
+        }
+        // Recursively check if the left and right subtrees are identical
+        return isSameTree(p.left, q.left) && isSameTree(p.right, q.right);
+
+
+        
+    }
+}
