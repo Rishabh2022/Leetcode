3e3b2be7b78443f9167c81759321857c62349cc4
Rishabh <78684570+Rishabh2022@users.noreply.github.com>
2024-10-06 14:51:30 +0530

    [620] [Time Beats: 26.12%] - LeetPush

---
+++ sql/620-Not-Boring-Movies.sql
@@ -0,0 +1,5 @@
+# Write your MySQL query statement below
+
+
+#https://leetcode.com/problems/not-boring-movies/?envType=study-plan-v2&envId=top-sql-50
+select * from Cinema where id%2<>0 and description<>\boring\ order by rating desc
